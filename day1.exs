3b7d2b79ed4f6216e36c980e1682794e4f31394d
Simon Ekström <simon.ekstrom@gmail.com>
2024-12-07 21:28:44 +0100

    day1

---
+++ day1.exs
@@ -0,0 +1,49 @@
+defmodule Day1 do
+  def process_line(line) do
+    line
+    |> String.split()
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  def parse_input(file) do
+    file
+    |> File.read!()
+    |> String.split("\n", trim: true)
+    |> Enum.map(&process_line/1)
+    |> Enum.reduce({[], []}, fn [l, r], {left, right} -> {[l | left], [r | right]} end)
+  end
+
+  def part1(left, right) do
+    {left, right}
+    |> then(fn {left, right} -> {Enum.sort(left), Enum.sort(right)} end)
+    # Calculate distances
+    |> then(fn {left, right} -> 
+      Enum.zip(left, right)
+      |> Enum.map(fn {l, r} -> abs(r - l) end)
+    # Sum all distances
+    |> Enum.sum()
+    end)
+  end
+  
+  def part2(left, right) do
+    {left, right}
+    # Multiply a[i] by freq of a[i] in b
+    |> then(fn {left, right} ->
+      freq = Enum.frequencies(right)
+      Enum.map(left, fn x -> x * Map.get(freq, x, 0) end)
+    end)
+    |> Enum.sum()
+  end
+
+end
+
+{left, right} = Day1.parse_input("../aoc-2024/src/input/day1.txt")
+
+ans1 = Day1.part1(left, right)
+IO.puts("Part 1: #{ans1}")
+
+ans2 = Day1.part2(left, right)
+IO.puts("Part 2: #{ans2}")
+
+
+
