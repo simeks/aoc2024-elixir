ac09c933f2a7b8e454773d5d442aaa30d238ef06
Simon Ekström <simon.ekstrom@gmail.com>
2024-12-09 00:19:40 +0100

    day5 part2

--- day5.exs
+++ day5.exs
@@ -25,6 +25,37 @@ defmodule Day5 do
     }
   end
 
+  def topologicalSort(rules) do
+    # Does topological sorting, assuming rules are a DAG
+
+    adjacency = Enum.reduce(rules, %{}, fn {a, b}, acc ->
+      Map.update(acc, a, [b], fn x -> [b | x] end)
+      |> Map.put_new(b, [])
+    end)
+
+    nodes = Map.keys(adjacency)
+
+    # DFS
+    Enum.reduce(nodes, {MapSet.new(), []}, fn node, {visited, sorted} ->
+      visit(node, adjacency, visited, sorted)
+    end)
+    |> elem(1)
+  end
+
+  def visit(node, adjancency, visited, sorted) do
+    cond do
+      MapSet.member?(visited, node) -> {visited, sorted}
+      true ->
+        visited = MapSet.put(visited, node)
+        {visited, sorted} = Enum.reduce(Map.get(adjancency, node), {visited, sorted},
+          fn neighbor, {visited, sorted} ->
+            visit(neighbor, adjancency, visited, sorted)
+          end
+        )
+        {visited, [node | sorted]}
+    end
+  end
+
   def valid?([], _) do
     true
   end
@@ -50,7 +81,6 @@ defmodule Day5 do
         |> Enum.with_index()
         |> Map.new()
 
-
         {
           valid?(rules, indices),
           Enum.at(report, Integer.floor_div(length(report), 2))
@@ -62,10 +92,50 @@ defmodule Day5 do
     end)
   end
 
+  def part2(reports, rules) do
+    Enum.map(reports, fn report ->
+        numbers = MapSet.new(report)
+
+        # Filter out relevant rules
+        rules = Enum.filter(rules, fn {a, b} ->
+          MapSet.member?(numbers, a) and MapSet.member?(numbers, b)
+        end)
+
+        # Map of number -> position
+        indices = report
+        |> Enum.with_index()
+        |> Map.new()
+
+        {
+          valid?(rules, indices),
+          report,
+        }
+      end)
+    |> Enum.filter(fn {valid, _} -> !valid end)
+    |> Enum.map(&(elem(&1, 1)))
+    |> Enum.map(fn report ->
+        numbers = MapSet.new(report)
+
+        # Filter out relevant rules
+        rules = Enum.filter(rules, fn {a, b} ->
+          MapSet.member?(numbers, a) and MapSet.member?(numbers, b)
+        end)
+
+        topologicalSort(rules)
+        |> Enum.at(Integer.floor_div(length(report), 2))
+      end)
+    |> Enum.reduce(0, fn
+      n, acc -> acc + n
+    end)
+  end
+
 end
 
 {reports, rules} = Day5.parse_input("inputs/day5.txt")
 
 ans1 = Day5.part1(reports, rules)
 IO.puts("Part 1: #{ans1}")
+
+ans2 = Day5.part2(reports, rules)
+IO.puts("Part 2: #{ans2}")
 
